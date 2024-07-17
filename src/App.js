06a61721426cb677682e242bf127a882a9945c3a
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-07-17 18:33:16 +0330

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Set to 0
+      </button>
+      <h1>{count}</h1>
+    </div>
+  );
 }
 
 export default App;
